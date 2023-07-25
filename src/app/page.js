5fbc29b5b59380be8c744614ff950966804a34c2
BestChennn <bestchenbestchen@gmail.com>
2023-07-25 22:56:08 +0700

    Completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,45 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { useState } from "react";
+import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 
 export default function RandomUserPage() {
   //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+  const [isFirstLoad, setIsFristLoanding] = useState(true);
+
+  useEffect(() => {
+    if (isFirstLoad) {
+      setIsFristLoanding(false);
+      return;
+    }
+    if (!isFirstLoad) {
+      const strTasks = JSON.stringify(genAmount);
+      localStorage.getItem("task", strTasks);
+    }
+  }, [genAmount]);
+
+  useEffect(() => {
+    const strTasks = JSON.parse(localStorage.getItem("task"));
+    if (strTasks == null) {
+      return;
+    }
+    setGenAmount(parseInt(strTasks));
+    const fetchData = async () => {
+      setIsLoading(true);
+      const resp = await axios.get(
+        `https://randomuser.me/api/?results=${strTasks}`
+      );
+      const users = resp.data.results.map(cleanUser);
+      setUsers(users);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -15,10 +47,8 @@ export default function RandomUserPage() {
       `https://randomuser.me/api/?results=${genAmount}`
     );
     setIsLoading(false);
-    const users = resp.data.results;
-    //Your code here
-    //Process result from api response with map function. Tips use function from /src/libs/cleanUser
-    //Then update state with function : setUsers(...)
+    const users = resp.data.results.map(cleanUser);
+    setUsers(users);
   };
 
   return (
@@ -40,7 +70,16 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users &&
+        !isLoading &&
+        users.map((user) => (
+          <UserCard
+            name={user.name}
+            imgUrl={user.imgUrl}
+            address={user.address}
+            email={user.email}
+          />
+        ))}
     </div>
   );
 }
