3c56719fe7950fb539fb7981d186a97f4e8faaa4
BestChennn <bestchenbestchen@gmail.com>
2023-07-25 23:24:10 +0700

    Completed

--- src/app/page.js
+++ src/app/page.js
@@ -55,6 +55,7 @@ export default function RandomUserPage() {
         !isLoading &&
         users.map((user) => (
           <UserCard
+            key={user.email}
             name={user.name}
             imgUrl={user.imgUrl}
             address={user.address}
