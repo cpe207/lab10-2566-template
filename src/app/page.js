a2ba85d97ceb70d20050ea6111a2cf355d2d237d
BestChennn <bestchenbestchen@gmail.com>
2023-07-25 23:12:22 +0700

    Completed

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,17 @@ import axios from "axios";
 import { UserCard } from "@/components/UserCard";
 import { cleanUser } from "@/libs/cleanUser";
 
+export default function RandomUserPage() {
+  //user = null or array of object
+  const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [genAmount, setGenAmount] = useState(1);
+  const [isFirstLoad, setIsFristLoanding] = useState(true);
+
+  useEffect(() => {
+    if (isFirstLoad) {
+      setIsFristLoanding(false);
+      return;
     }
     if (!isFirstLoad) {
       const strTasks = JSON.stringify(genAmount);
